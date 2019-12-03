dd826f5000bc662e3762012fc819f476aa5ae227
David Goldstein <david.goldstein@avalara.com>
2019-12-03 16:36:41 -0700

    Flesh out customer service tests

--- test/customerServiceTests.js
+++ test/customerServiceTests.js
@@ -10,7 +10,8 @@ const AgentRepo = require('../service/agentRepo').AgentRepo;
 const TestData = require('./customerTestData').CustomerTestData;
 
 describe('Customer service', function () {
-    const agentId = 42;
+    const agentId = 42,
+        customerId = 420;
 
     let sut,
         validator,
@@ -20,6 +21,8 @@ describe('Customer service', function () {
         agentExistsStub,
         customerExistsStub,
         addCustomerStub,
+        updateCustomerStub,
+        getCustomerStub,
         testData;
 
     this.beforeEach(function () {
@@ -30,6 +33,8 @@ describe('Customer service', function () {
         agentExistsStub = sinon.stub(agentRepo, 'agentExists');
         customerExistsStub = sinon.stub(customerRepo, 'customerExists');
         addCustomerStub = sinon.stub(customerRepo, 'addCustomer');
+        updateCustomerStub = sinon.stub(customerRepo, 'updateCustomer');
+        getCustomerStub = sinon.stub(customerRepo, 'getCustomer');
         sut = new Sut(validator, customerRepo, agentRepo);
         testData = new TestData();
     });
@@ -84,6 +89,61 @@ describe('Customer service', function () {
             await sut.addCustomer(newCustomer, agentId);
 
             assert.equal(addCustomerStub.called, true);
+        });
+    });
+
+    describe('addOrUpdateCustomer', function () {
+        it ('should throw if the agent does not exist', async function () {
+            const customer = testData.makeValidCustomer();
+            agentExistsStub.resolves(false);
+            
+            await assert.rejects(() => sut.addOrUpdateCustomer(customer, customerId, agentId), { name: 'UnprocessableError' });
+        });
+        
+        it ('should throw for invalid customer', async function () {
+            const customer = testData.makeValidCustomer();
+            customer.company = null;
+            agentExistsStub.resolves(true);
+            
+            await assert.rejects(() => sut.addOrUpdateCustomer(customer, customerId, agentId), { name: 'InvalidResourceError' });
+        });
+
+        it ('should call update customer if the customer exists', async function () {
+            const customer = testData.makeValidCustomer();
+            agentExistsStub.resolves(true);
+            customerExistsStub.resolves(true);
+            
+            await sut.addOrUpdateCustomer(customer, customerId, agentId);
+
+            assert.equal(updateCustomerStub.called, true);
+        });
+
+        it ('should call add customer if the customer does not exist', async function () {
+            const customer = testData.makeValidCustomer();
+            agentExistsStub.resolves(true);
+            customerExistsStub.resolves(false);
+            
+            await sut.addOrUpdateCustomer(customer, customerId, agentId);
+
+            assert.equal(addCustomerStub.called, true);
+        });
+    });
+
+    describe('getCustomer', function () {
+        it ('should throw if no customer is returned from the repository', async function () {
+            const customer = null;
+            getCustomerStub.resolves(customer);
+    
+            await assert.rejects(() => sut.getCustomer(customerId, agentId), { name: 'NotFoundError' });
+        });
+
+        it ('should return the customer returned from the repository', async function () {
+            const customer = testData.makeValidCustomer();
+            getCustomerStub.resolves(customer);
+
+            const actual = await sut.getCustomer(customerId, agentId);
+
+            assert.deepEqual(actual, customer);
         });
     });
 });
